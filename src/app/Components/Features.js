2a79353f715d06b114fe223ce39c02d176d0148e
zarrar79 <shahzarrar79@gmail.com>
2024-05-04 01:46:31 +0500

    features' section

---
+++ src/app/Components/Features.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import bigImage from '../../../public/assets/b_image_feature.svg'
+import shield from '../../../public/assets/shield.svg'
+import mail from '../../../public/assets/mail.svg'
+import kristal from '../../../public/assets/kristal.svg'
+const Features = () => {
+  return (
+    <>
+    {/* background */}
+    <div className='bg-[#1e232e] overflow-hidden mt-[6rem] font-Inter'>
+        {/* fluid */}
+        <div className='max-w-[1231px] mx-auto'>
+     {/* section */}
+     <div className="mt-[6rem] flex justify-between">
+          {/* left */}
+          <div>
+            {/* big-text */}
+            <div className="text-white font-[900] text-[40px] leading-[46px] mt-[58px]">
+            Basic chatbots have<br></br> <span className='text-[#F1D598]'>no business</span> representing<br></br> <span className='text-[#C4ADEB]'>your business</span>
+            </div>
+            {/* sm-text */}
+            <div className="w-[40rem] font-[400] text-[18px] text-[white] leading-[27px] mt-9">
+            Ditch code-heavy, old-school scripted chatbots.<br></br> Powered by Ada’s Reasoning Engine™, an AI Agent<br></br> reasons through the best solution to deliver responses<br></br> that are relevant, accurate and safe. Every single time.
+            </div>
+            {/* buttons */}
+            <div className="flex mt-9 hero_btn">
+              {/* button 1 */}
+              <div className="font-[800] text-[16px] text-[white]"><button className="rounded-[8px] px-[25px] py-[12px] bg_btns">Get a demo</button></div>
+              <div className="font-[800] text-[16px] text-[white]"><button className="btn_2 ml-7 border-[#9977D4] border-[2px] rounded-[8px] px-[23px] py-[10px]">Learn More</button></div>
+            </div>
+          </div>
+          {/* right */}
+          <div className="hero_big_img"><img className="max-w-[142%] ml-3" src={bigImage.src}></img></div>
+        </div>
+        {/* grid */}
+        <div className='grid grid-cols-4 gap-8 mt-24'>
+            {/* box 1 */}
+            <div className='border border-slate-800/60 rounded-lg box_shadow_card_feature px-8 py-10'>
+                {/* inner parent */}
+                <div>
+                    {/* img */}
+                    <div><img src={shield.src}></img></div>
+                    {/* head */}
+                    <div className='mt-2 text-[#fff] font-[700] text-[20px]'>Forrester</div>
+                    {/* text */}
+                    <div className='mt-2 leading-[20px] font-[400] text-[16px] text-[#fff]'>Conversational AI Wave Challenger</div>
+                </div>
+            </div>
+            {/* box 2 */}
+            <div className='border border-slate-800/60 rounded-lg box_shadow_card_feature px-8 py-10'>
+                {/* inner parent */}
+                <div>
+                    {/* img */}
+                    <div><img src={shield.src}></img></div>
+                    {/* head */}
+                    <div className='mt-2 text-[#fff] font-[700] text-[20px]'>Deloitte</div>
+                    {/* text */}
+                    <div className='mt-2 leading-[20px] font-[400] text-[16px] text-[#fff]'>Technology Fast 500</div>
+                </div>
+            </div>
+            {/* box 3 */}
+            <div className='border border-slate-800/60 rounded-lg box_shadow_card_feature px-8 py-10'>
+                {/* inner parent */}
+                <div>
+                    {/* img */}
+                    <div><img src={shield.src}></img></div>
+                    {/* head */}
+                    <div className='mt-2 text-[#fff] font-[700] text-[20px]'>Fast Company</div>
+                    {/* text */}
+                    <div className='mt-2 leading-[20px] font-[400] text-[16px] text-[#fff]'>Next Big Things in Tech</div>
+                </div>
+            </div>
+            {/* box 4 */}
+            <div className='border border-slate-800/60 rounded-lg box_shadow_card_feature px-8 py-10'>
+                {/* inner parent */}
+                <div>
+                    {/* img */}
+                    <div><img src={mail.src}></img></div>
+                    {/* head */}
+                    <div className='mt-2 text-[#fff] font-[700] text-[20px]'>Globe & Mail</div>
+                    {/* text */}
+                    <div className='mt-2 leading-[20px] font-[400] text-[16px] text-[#fff]'>Top Growing Companies</div>
+                </div>
+            </div>
+        </div>
+        {/* flex for b-image and text */}
+        <div className='flex items-center justify-between'>
+            {/* left */}
+            <div><img className='ml-[-248px] max-w-[210%]' src={kristal.src}></img></div>
+            {/* right */}
+            <div className='text-[white]'>
+                <div className='pl-[100px]'>
+              {/* head */}
+              <div className='font-[900] text-[40px] text-[#fff]'>Resolve more phone calls with voice AI</div>
+              {/* sm-text */}
+              <div className='font-[400] text-[18px]'>Stop delivering a disjointed customer experience. Hire an AI Agent that can communicate consistently in any channel. Seamlessly add intelligent phone automation using the same knowledge content, all in a single platform.</div>
+              {/* banner */}
+            </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </>
+  )
+}
+export default Features;
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 import React from 'react'
 import  Hero  from './Components/Hero';
 import Testimonials from './Components/Testimonials'
+import Features from './Components/Features'
 const page = () => {
   return (
     <>
      <Hero/>
      <Testimonials/>
+     <Features/>
     </>
   )
 }
