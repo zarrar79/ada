765ce5f44a21127590028e8749ce32d2b85575ca
zarrar79 <shahzarrar79@gmail.com>
2024-05-05 00:02:45 +0500

    feature page updates

--- src/app/Components/Features.js
+++ src/app/Components/Features.js
@@ -10,18 +10,19 @@ const Features = () => {
     {/* background */}
     <div className='bg-[#1e232e] overflow-hidden mt-[6rem] font-Inter'>
         {/* fluid */}
+        <div className='container mx-auto'>
         <div className='max-w-[1231px] mx-auto'>
      {/* section */}
-     <div className="mt-[6rem] flex justify-between">
+     <div className="mt-[5.5rem] grid gap-9 grid-cols-2 mx-auto items-center">
           {/* left */}
           <div>
             {/* big-text */}
-            <div className="text-white font-[900] text-[40px] leading-[46px] mt-[58px]">
+            <div className="b_text_feature text-white font-[900] text-[40px] leading-[46px]">
             Basic chatbots have<br></br> <span className='text-[#F1D598]'>no business</span> representing<br></br> <span className='text-[#C4ADEB]'>your business</span>
             </div>
             {/* sm-text */}
-            <div className="w-[40rem] font-[400] text-[18px] text-[white] leading-[27px] mt-9">
-            Ditch code-heavy, old-school scripted chatbots.<br></br> Powered by Ada’s Reasoning Engine™, an AI Agent<br></br> reasons through the best solution to deliver responses<br></br> that are relevant, accurate and safe. Every single time.
+            <div className="sm_text_feature pr-[152px] w-[40rem] font-[400] text-[18px] text-[white] leading-[27px] mt-9">
+            Ditch code-heavy, old-school scripted chatbots. Powered by Ada’s Reasoning Engine™, an AI Agent reasons through the best solution to deliver responses that are relevant, accurate and safe. Every single time.
             </div>
             {/* buttons */}
             <div className="flex mt-9 hero_btn">
@@ -31,10 +32,10 @@ const Features = () => {
             </div>
           </div>
           {/* right */}
-          <div className="hero_big_img"><img className="max-w-[142%] ml-3" src={bigImage.src}></img></div>
+          <div className="bg_img_feature"><img className="max-w-[141%] ml-4" src={bigImage.src}></img></div>
         </div>
         {/* grid */}
-        <div className='grid grid-cols-4 gap-8 mt-24'>
+        <div className='grid_card_feature grid grid-cols-4 gap-8 mt-24'>
             {/* box 1 */}
             <div className='border border-slate-800/60 rounded-lg box_shadow_card_feature px-8 py-10'>
                 {/* inner parent */}
@@ -85,16 +86,16 @@ const Features = () => {
             </div>
         </div>
         {/* flex for b-image and text */}
-        <div className='flex items-center justify-between mt-[54.5px]'>
+        <div className='grid items-center grid-cols-2 mt-[54.5px]'>
             {/* left */}
-            <div className='w-[30%]'><img className='mt-[45px] ml-[-262px] max-w-[229%]' src={kristal.src}></img></div>
+            <div><img className='img_feature_sec mt-[45px] max-w-[142%] ml-[-280px]' src={kristal.src}></img></div>
             {/* right */}
-            <div className='text-[white] w-[70%]'>
-                <div className='max-w-[56%] mx-auto ml-[283px]'>
+            <div className='text-[white]'>
+                <div className='px-[67px] mt-5 head_txt_feature'>
               {/* head */}
               <div className='leading-[46px] font-[900] text-[40px] text-[#fff]'>Resolve more phone calls with voice AI</div>
               {/* sm-text */}
-              <div className='mt-[38px] font-[400] leading-[27px] text-[18px]'>Stop delivering a disjointed customer experience. Hire an AI Agent that can communicate consistently in any channel. Seamlessly add intelligent phone automation using the same knowledge content, all in a single platform.</div>
+              <div className='sm_text_feature_2 mt-[38px] font-[400] leading-[27px] text-[18px]'>Stop delivering a disjointed customer experience. Hire an AI Agent that can communicate consistently in any channel. Seamlessly add intelligent phone automation using the same knowledge content, all in a single platform.</div>
             {/* banner */}
             <div className= "font-[600] mt-[38px] text-[14px] text-[#1E232E] bg-[#e3d4fc] flex items-center w-[249px] rounded-[16px] px-[20px] py-[5px] banner_shadow">
               {/* text */}
@@ -107,6 +108,7 @@ const Features = () => {
             </div>
             </div>
         </div>
+    </div>
     </div>
     </div>
     </>
